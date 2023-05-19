d8ecd3c292d9d34e56b41f6d3f027d7cb612d7f6
Sabith Fariq <sabithfariq@gmail.com>
2023-05-20 00:12:42 +0530

    Animations on TEXT and Logo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// window.addEventListener('scroll', function() {
+//     var leftBox = document.getElementById('parallelogram');
+//     var offset = window.pageYOffset;
+//     var scaleFactor = 0.5; 
+  
+//     leftBox.style.transform = 'skewX(-20deg) scaleX(' + (1 + scaleFactor * offset / 50) + ')';
+//     leftBox.querySelector('.content').style.transform = 'skewX(20deg)';
+//   });
